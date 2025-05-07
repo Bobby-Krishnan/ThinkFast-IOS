6bc9fda30ec1ee9f8c674a8149701a4f9667de82
Bobby-Krishnan <Abhishek.v.krishnan@gmail.com>
2025-05-07 15:06:53 -0400

    webview

--- ThinkFast-IOS/ThinkFast-IOS/ThinkFastWebView.swift
+++ ThinkFast-IOS/ThinkFast-IOS/ThinkFastWebView.swift
@@ -1,8 +1,18 @@
-//
-//  ThinkFastWebView.swift
-//  ThinkFast-IOS
-//
-//  Created by Bobby Krishnan on 5/7/25.
-//
+import SwiftUI
+import WebKit
 
-import Foundation
+// WebView using WKWebView for ThinkFast React app
+struct ThinkFastWebView: UIViewRepresentable {
+    let url: URL
+    
+    // Create the WebView (WKWebView)
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+
+    // Update WebView when necessary (not used for now)
+    func updateUIView(_ webView: WKWebView, context: Context) {}
+}
